edfff845f3e9cb04c9e049cbe1f845615569d632
Martin Raunkjær Andersen <mar@commentor.dk>
2019-10-25 15:00:19 +0200

    removed autocomplete for mardl

--- .autocomplete.sh
+++ .autocomplete.sh
@@ -7,12 +7,8 @@ _docs_completion='$(ls ~/bin/documentation)'
 complete -W "${_docs_completion}" 'docs'
 
 # mardl (note: only gets what was in marand:/var/www/html/share when opening shell)
-_mardl_completion=$(ssh marand@marand.dk "ls -tA /var/www/html/share")
-complete -W "${_mardl_completion}" 'mardl'
-
-
-
-
+#_mardl_completion=$(ssh marand@marand.dk "ls -tA /var/www/html/share")
+#complete -W "${_mardl_completion}" 'mardl'
 
 # to add have normal bash options as well:
 #complete -W "${options}" -o bashdefault -o default 'scriptname'
