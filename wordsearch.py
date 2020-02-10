4251c06ee66785ab77fe660fa8a9a714eae632e7
myrionsc <marpht23@gmail.com>
2020-02-10 22:31:36 +0100

    words

--- wordsearch.py
+++ wordsearch.py
@@ -3,16 +3,30 @@
 import os
 import sys
 
-if len(sys.argv) < 2:
+def print_instructions():
     print("Finds english words containing a given substring")
     print("first arg should be a string")
+    print("last arg can be flags:")
+    print("  -b    Words beginning with arg1")
+    print("  -e    Words ending with arg1")
+
+if len(sys.argv) < 2:
+    print_instructions()
     exit()
 
 term = sys.argv[1]
 
 wordsfile = open("words.txt", "r")
 words = wordsfile.readlines()
+matches = ""
 
-matches = [w.strip() for w in words if w.lower().find(term) != -1]
+if len(sys.argv) == 3 and sys.argv[2] == "-b":
+    matches = [w.strip() for w in words if w.lower().startswith(term)]
+elif len(sys.argv) == 3 and sys.argv[2] == "-e":
+    matches = [w.strip() for w in words if w.lower().strip().endswith(term)]
+else:
+    matches = [w.strip() for w in words if w.lower().find(term) != -1]
+
 print(", ".join(matches))
+
 
